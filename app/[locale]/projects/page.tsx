a2054f7008c1a30755023e15b7ff3827d4dd9dad
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-06-26 15:52:10 +0300

    refactor: remove unused translation logic and clean up localization in Projects page
    - Eliminated the unused `getTranslations` import from the Projects page for improved code clarity.
    - Cleaned up the English JSON file by removing empty string entries to enhance localization consistency.

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,9 +1,7 @@
 import ProjectsHero from "@/components/projects/ProjectsHero";
 import ProjectsPagination from "@/components/projects/ProjectsPagination";
-import { getTranslations } from "next-intl/server";
 
 const page = async () => {
-  const t = await getTranslations("projects");
   return (
     <div className="w-full">
       <ProjectsHero />
